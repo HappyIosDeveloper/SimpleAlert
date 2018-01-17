036b5ecc3b72f81f94be33b8fc4915fbfd7d6f23
Alfredo <lovlyboy@gmail.com>
2018-01-17 10:23:01 +0330

    Added extension file.

---
+++ SimpleAlertExtension.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleAlertExtension.swift
+//  WHATEVER
+//
+//  Created by GCO2(Ahmadreza) on 1/17/18.
+//  Copyright © 2018 GCO. All rights reserved.
+//
+
+import UIkit
+
+extension UIViewController {
+    
+    func simpleAlert(title: String = "Attention!", message: String) {
+        let alertController = UIAlertController(title: title, message: "\n" + message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+}
